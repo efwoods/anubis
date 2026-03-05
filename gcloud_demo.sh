29468b90837d0186df1b56fbe71b0158d7ca95ff
Evan Woods <22337004+efwoods@users.noreply.github.com>
2026-03-05 16:20:47 -0500

    update

--- gcloud_demo.sh
+++ gcloud_demo.sh
@@ -1,4 +1,4 @@
-gcloud compute instances create anubis-demo \
+gcloud compute instances create nn-demo \
   --machine-type=e2-micro \
   --zone=us-central1-a \
   --image-family=debian-12 \
@@ -11,5 +11,13 @@ curl -fsSL https://get.docker.com | sh
 sudo usermod -aG docker $USER
 sudo apt-get install -y docker-compose-plugin
 newgrp docker
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
 
 gcloud compute ssh anubis-demo --zone=us-centra1-a

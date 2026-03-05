1ef96c15426ff59a892040f637b17e8635b8f195
Evan Woods <22337004+efwoods@users.noreply.github.com>
2026-03-05 16:07:57 -0500

    stable commit for test deployment

--- gcloud_demo.sh
+++ gcloud_demo.sh
@@ -1,1 +1,15 @@
+gcloud compute instances create anubis-demo \
+  --machine-type=e2-micro \
+  --zone=us-central1-a \
+  --image-family=debian-12 \
+  --image-project=debian-cloud \
+  --boot-disk-size=100GB \
+  --tags=http-server,https-server
+
+
+curl -fsSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
+sudo apt-get install -y docker-compose-plugin
+newgrp docker
+
 gcloud compute ssh anubis-demo --zone=us-centra1-a

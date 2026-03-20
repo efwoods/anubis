e06f538aba2d679ff568412ebf67d32ecb26262a
Evan Woods <22337004+efwoods@users.noreply.github.com>
2026-03-19 21:34:06 -0400

    update

--- gcloud_demo.sh
+++ gcloud_demo.sh
@@ -13,16 +13,16 @@
 gcloud compute ssh neural-nexus-api-demo --zone=us-centra1-a
 
 # On the VM:
-# curl -fsSL https://get.docker.com | sh
-# sudo usermod -aG docker $USER
-# sudo apt-get install -y docker-compose-plugin
-# newgrp docker
+curl -fsSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
+sudo apt-get install -y docker-compose-plugin
+newgrp docker
 
-# (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
-# && sudo mkdir -p -m 755 /etc/apt/keyrings \
-# && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-# && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-# && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-# && sudo apt update \
-# && sudo apt install gh -y
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
 

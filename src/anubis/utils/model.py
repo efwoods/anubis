b4e86d818aa59172ea512e5f0a1e63d7e5e40fb2
Evan Woods <22337004+efwoods@users.noreply.github.com>
2026-04-02 15:21:04 -0400

    updated usefule models

--- src/anubis/utils/model.py
+++ src/anubis/utils/model.py
@@ -8,7 +8,7 @@ from typing import Optional
 
 from pydantic import BaseModel
 from langchain_together import ChatTogether
-
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
 """ TODO: Prevent Rate Limiting and Token Limiting Errors and Handle Message Failures """
 
@@ -23,6 +23,7 @@ def init_model(context: Optional[GlobalContext] = GlobalContext(),
     base_url = context.llm_provider_base_url
     api_key = context.llm_provider_api_key
     dev = context.dev
+    model_provider = context.model_provider
 
     logger.info(f"dev: {dev}")
     logger.info(f"api_key: {api_key}")
@@ -31,29 +32,52 @@ def init_model(context: Optional[GlobalContext] = GlobalContext(),
 
     # from langchain_openai import ChatOpenAI
 
-    if response_format is None:
-        model = ChatTogether(
-                    model = model_name,
-                    base_url = base_url,
-                    temperature=0.1,
-                    top_p=0.1,
-                    api_key = api_key,
-                ).bind_tools(
-                    # method='json_schema', 
-                    tools=tools, 
-                    tool_choice=tool_choice, # auto: zero or more tools
-                    # strict=True, # model output will be guaranteed to match the schema
-                    # include_raw=True # model response (JSON e.g.) and the parsed response (Pydantic e.g.) will be returned
-                )
-    else: 
-        model = ChatTogether(
-            model = model_name,
-            base_url = base_url,
-            temperature=0.1,
-            top_p=0.1,
-            api_key = api_key,
-        )
-        model = model.with_structured_output(schema=response_format)
+    if model_provider == "TOGETHER":
+        if response_format is None:
+            model = ChatTogether(
+                        model = model_name,
+                        base_url = base_url,
+                        temperature=0.1,
+                        top_p=0.1,
+                        api_key = api_key,
+                    ).bind_tools(
+                        # method='json_schema', 
+                        tools=tools, 
+                        tool_choice=tool_choice, # auto: zero or more tools
+                        # strict=True, # model output will be guaranteed to match the schema
+                        # include_raw=True # model response (JSON e.g.) and the parsed response (Pydantic e.g.) will be returned
+                    )
+        else: 
+            model = ChatTogether(
+                model = model_name,
+                base_url = base_url,
+                temperature=0.1,
+                top_p=0.1,
+                api_key = api_key,
+            )
+            model = model.with_structured_output(schema=response_format)
+    elif model_provider == "NVIDIA":
+        if response_format is None:
+            model = ChatNVIDIA(
+                            model = model_name,
+                            temperature=0.1,
+                            top_p=0.1,
+                            api_key = api_key,
+                        ).bind_tools(
+                            # method='json_schema', 
+                            tools=tools, 
+                            tool_choice=tool_choice, # auto: zero or more tools
+                            # strict=True, # model output will be guaranteed to match the schema
+                            # include_raw=True # model response (JSON e.g.) and the parsed response (Pydantic e.g.) will be returned
+                        )
+        else: 
+            model = ChatNVIDIA(
+                model = model_name,
+                temperature=0.1,
+                top_p=0.1,
+                api_key = api_key,
+            )
+            model = model.with_structured_output(schema=response_format)
     
     return model
 
